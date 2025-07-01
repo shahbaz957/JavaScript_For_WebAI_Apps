13eb50899932c1abef09644eee4af073650acd0c
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2025-06-30 20:31:01 -0700

    Add files via upload

---
+++ zero_1_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version of javascript
+
+// alert( 3+3 )  we are using nodejs not the browser
+
+console.log(3 +
+     3)   // code readability should be high so this is not a best practice
+
+
+console.log(5*5)
+
+let name = "Shahbaz"
+let age = 18
+let isLoggedIn = true
+
+//                                          <-- DATATYPES -->
+
+// Number => 2 to the power of 53 (range of number)
+// bigInt => for larger number than Number dtype
+// string => "" or ''
+// boolean => true / false
+// null => standalone value
+// undefined => value is not defined yet
+// symbol => for uniqueness
+// object
+
+console.log(typeof age);
+console.log(2**3);
