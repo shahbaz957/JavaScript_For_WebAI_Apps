261b0ceeb3f1be49803b2385a44c408f3e1d5d74
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2025-06-30 23:47:32 -0700

    Add files via upload

---
+++ zero_1_basics/operations.js
@@ -0,0 +1,13 @@
+// let value = 3
+// let neg_val = -value;
+// console.log(neg_val);
+
+console.log("1" + 2);   // javascript prefernece according to browser engine prefernece
+console.log(1 + 2 + "3");   // this is just according to ecma script 
+console.log(true)
+console.log(+true)
+console.log(+"")  // because the bool value for "" is false 
+
+let gameCounter = 100;
+console.log(gameCounter++);
+console.log(gameCounter)
