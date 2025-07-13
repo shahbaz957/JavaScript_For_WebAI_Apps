1eb3312d9b22bd1cd2af67d743733e79bb5c5cf2
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2025-07-13 16:44:58 +0500

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,25 @@
+
+async function getAllusers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getAllusers();
+
+// Doing by promise Method
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
